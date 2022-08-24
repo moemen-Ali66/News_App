b0476a167a56359a24d9f059d10174dd3bdaf2af
pc <m>
2022-08-25 01:58:08 +0200

    m

--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -64,13 +64,13 @@ class NewsCubit extends Cubit<NewsStates>{
       {
         'country':'eg',
         'category':'sports',
-        'apiKey':'6420a625766848a8b5ebad345c636309',
+        'apiKey':'839f8b514c5644f496f9a60b15c0951f',
       },
     ).then((value)
     {
       //print(value.data['articles'][0]['title']);
-      business = value.data['articles'];
-      print(business[0]['title']);
+      Sports = value.data['articles'];
+      print(Sports[0]['title']);
 
       emit(NewsgetSportsSuccessStates());
     }).catchError((error){
@@ -94,8 +94,8 @@ class NewsCubit extends Cubit<NewsStates>{
     ).then((value)
     {
       //print(value.data['articles'][0]['title']);
-      business = value.data['articles'];
-      print(business[0]['title']);
+      Science = value.data['articles'];
+      print(Science[0]['title']);
 
       emit(NewsgetScienceSuccessStates());
     }).catchError((error){
