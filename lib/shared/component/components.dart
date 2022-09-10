a33d0766e47c2946c65cc5de6fc4bcc7550c616e
pc <m>
2022-09-10 05:22:49 +0200

    m

--- lib/shared/component/components.dart
+++ lib/shared/component/components.dart
@@ -1,40 +1,83 @@
 import 'package:flutter/material.dart';
-Widget buildItem(articles,context)=> Padding(
-  padding:  EdgeInsets.all(10.0),
-  child: Row(
-    children: [
-      Container(
-        height: 140,
-        width: 120,
-        decoration: BoxDecoration(
-          borderRadius:BorderRadius.circular(20.0) ,
-          image:DecorationImage(
-            image: NetworkImage('${articles['urlToImage']}',),onError: (context,exeption){
-              Image.asset('images/no-image-icon-15.png');
-          },
-            fit:BoxFit.cover,
+import 'package:news_app/moduels/web_view/WebViewScreen.dart';
+Widget buildItem(articles,context)=> InkWell(
+  // onTap: (){
+  //   Navigator.push(context, MaterialPageRoute(builder: (context)=>WebViewScreen(articles['url'])));
+  // },
+  child:   Padding(
+
+    padding:  EdgeInsets.all(10.0),
+
+    child: Row(
+
+      children: [
+
+        Container(
+
+          height: 140,
+
+          width: 120,
+
+          decoration: BoxDecoration(
+
+            borderRadius:BorderRadius.circular(20.0) ,
+
+            image:DecorationImage(
+
+              image: NetworkImage('${articles['urlToImage']}',),onError: (context,exeption){
+
+                Image.asset('images/no-image-icon-15.png');
+
+            },
+
+              fit:BoxFit.cover,
+
+            ),
+
+          ),),
+
+        SizedBox(width: 20,),
+
+        Expanded(
+
+          child: Container(
+
+            height: 120,
+
+            child: Column(
+
+              mainAxisAlignment: MainAxisAlignment.start,
+
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+
+                Expanded(
+
+                  child: Text('${articles['title']}',
+
+                    style:Theme.of(context).textTheme.bodyText1,
+
+                    maxLines: 3,
+
+                    overflow:TextOverflow.ellipsis,),
+
+                ),
+
+                Text('${articles['publishedAt']}',style: TextStyle(color: Colors.grey),)
+
+              ],
+
+            ),
+
           ),
-        ),),
-      SizedBox(width: 20,),
-      Expanded(
-        child: Container(
-          height: 120,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Text('${articles['title']}',
-                  style:Theme.of(context).textTheme.bodyText1,
-                  maxLines: 3,
-                  overflow:TextOverflow.ellipsis,),
-              ),
-              Text('${articles['publishedAt']}',style: TextStyle(color: Colors.grey),)
-            ],
-          ),
+
         ),
-      ),
-    ],
+
+      ],
+
+    ),
+
   ),
 );
 Widget myDivider()=>Padding(
